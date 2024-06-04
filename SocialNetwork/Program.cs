d3a8bb3447a71ba05b9e1f4a317eccf0fccd9f5f
Ilia <isinetov@mail.ru>
2024-06-04 18:02:33 +0400

    Добавлена возможность входа в существующий аккаунт пользователя и возможность редактирования данных профиля.

--- SocialNetwork/Program.cs
+++ SocialNetwork/Program.cs
@@ -1,9 +1,10 @@
-﻿using SocialNetwork.BLL.Models;
+﻿using SocialNetwork.BLL.Exceptions;
+using SocialNetwork.BLL.Models;
 using SocialNetwork.BLL.Services;
 
-namespace SocialNetwork 
+namespace SocialNetwork
 {
-    class Program 
+    class Program
     {
         public static UserService userService = new UserService();
         static void Main(string[] args)
@@ -12,45 +13,135 @@ namespace SocialNetwork
 
             while (true)
             {
-                Console.Write("Для регистрации введите имя пользователя: ");
+                Console.WriteLine("Войти в профиль (нажмите 1)");
+                Console.WriteLine("Зарегистрироваться (нажмите 2)");
 
-                string firstname = Console.ReadLine();
-
-                Console.Write("фамилию: ");
-
-                string lastname = Console.ReadLine();
-
-                Console.Write("пароль: ");
-
-                string password = Console.ReadLine();
-
-                Console.Write("адрес электронной почты: ");
-
-                string email = Console.ReadLine();
-
-                var userRegistrationData = new UserRegistrationData()
+                switch (Console.ReadLine())
                 {
-                    FirstName = firstname,
-                    LastName = lastname,
-                    Password = password,
-                    Email = email
-                };
+                    case "1":
+                        {
+                            var authenticationData = new UserAuthenticationData();
+                            
+                            Console.Write("Введите почтовый адрес: ");
+                            authenticationData.Email = Console.ReadLine();
 
-                try
-                {
-                    userService.Register(userRegistrationData);
-                    Console.WriteLine("Регистрация прошла успешно!");
+                            Console.Write("Введите пароль: ");
+                            authenticationData.Password = Console.ReadLine();
+
+                            try
+                            {
+                                User user = userService.Authenticate(authenticationData);
+
+                                Console.ForegroundColor = ConsoleColor.Green;
+                                Console.WriteLine($"Вы успешно вошли в социальную сеть! Добро пожаловать {user.FirstName}");
+                                Console.ForegroundColor = ConsoleColor.White;
+
+                                while (true)
+                                {
+                                    Console.WriteLine("Просмотреть информацию о моём профиле (введите 1)");
+                                    Console.WriteLine("Редактировать мой профиль (введите 2)");
+                                    Console.WriteLine("Добавить в друзья (введите 3)");
+                                    Console.WriteLine("Написать сообщение (введите 4)");
+                                    Console.WriteLine("Выйти из профиля (введите 5)");
+
+                                    switch (Console.ReadLine())
+                                    {
+                                        case "1":
+                                            {
+                                                Console.ForegroundColor = ConsoleColor.Green;
+                                                Console.WriteLine("Информация о моем профиле");
+                                                Console.WriteLine($"Мой идентификатор: {user.Id}");
+                                                Console.WriteLine($"Меня зовут: {user.FirstName}");
+                                                Console.WriteLine($"Моя фамилия: {user.LastName}");
+                                                Console.WriteLine($"Мой пароль: {user.Password}");
+                                                Console.WriteLine($"Мой почтовый адрес: {user.Email}");
+                                                Console.WriteLine($"Ссылка на моё фото: {user.Photo}");
+                                                Console.WriteLine($"Мой любимый фильм: {user.FavoriteMovie}");
+                                                Console.WriteLine($"Моя любимая книга: {user.FavoriteBook}");
+                                                Console.ForegroundColor = ConsoleColor.White;
+                                                break;
+                                            }
+                                        case "2":
+                                            {
+                                                Console.Write("Меня зовут: ");
+                                                user.FirstName = Console.ReadLine();
+
+                                                Console.Write("Моя фамилия: ");
+                                                user.LastName = Console.ReadLine();
+
+                                                Console.Write("Ссылка на моё фото: ");
+                                                user.Photo = Console.ReadLine();
+
+                                                Console.Write("Мой любимый фильм: ");
+                                                user.FavoriteMovie = Console.ReadLine();
+
+                                                Console.Write("Моя любимая книга: ");
+                                                user.FavoriteBook = Console.ReadLine();
+
+                                                userService.Update(user);
+
+                                                Console.ForegroundColor = ConsoleColor.Green;
+                                                Console.WriteLine("Ваш профиль успешно обновлён!");
+                                                Console.ForegroundColor = ConsoleColor.White;
+
+                                                break;
+                                            }
+                                    }
+                                }
+
+                            }
+                            catch (WrongPasswordException)
+                            {
+                                Console.ForegroundColor = ConsoleColor.Red;
+                                Console.WriteLine("Пароль не корректный!");
+                                Console.ForegroundColor = ConsoleColor.White;
+                            }
+                            catch (UserNotFoundException)
+                            {
+                                Console.ForegroundColor = ConsoleColor.Red;
+                                Console.WriteLine("Пользователь не найден!");
+                                Console.ForegroundColor = ConsoleColor.White;
+                            }
+
+                            break;
+                        }
+
+                    case "2":
+                        {
+                            var userRegistrationData = new UserRegistrationData();
+
+                            Console.Write("Для создания нового профиля введите ваше имя: ");
+                            userRegistrationData.FirstName = Console.ReadLine();
+
+                            Console.Write("Ваша фамилия: ");
+                            userRegistrationData.LastName = Console.ReadLine();
+
+                            Console.Write("Пароль: ");
+                            userRegistrationData.Password = Console.ReadLine();
+
+                            Console.Write("Почтовый адрес: ");
+                            userRegistrationData.Email = Console.ReadLine();
+
+                            try
+                            {
+                                userService.Register(userRegistrationData);
+
+                                Console.ForegroundColor = ConsoleColor.Green;
+                                Console.WriteLine("Ваш профиль успешно создан. Теперь Вы можете войти в систему под своими учетными данными.");
+                                Console.ForegroundColor = ConsoleColor.White;
+                            }
+                            catch (ArgumentNullException)
+                            {
+                                Console.WriteLine("Введите корректное значение.");
+                            }
+                            catch (Exception)
+                            {
+                                Console.WriteLine("Произошла ошибка при регистрации.");
+                            }
+
+                            break;
+                        }
                 }
-                catch (ArgumentNullException)
-                {
-                    Console.WriteLine("Ошибка! В поля с данными введены некоректные значения.");
-                }
-                catch (Exception)
-                {
-                    Console.WriteLine("Ошибка! Не удалось создать учетную запись.");
-                }
-
-                Console.ReadLine();
             }
         }
     }
