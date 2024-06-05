4e941a6e1aca4508e2cb747905a4a13dbe2a493a
Ilia <isinetov@mail.ru>
2024-06-05 15:38:39 +0400

    Добавил уведомление об отсутствии входящих сообщений.

--- SocialNetwork/Views/UserReceivedMessageView.cs
+++ SocialNetwork/Views/UserReceivedMessageView.cs
@@ -15,6 +15,10 @@ namespace SocialNetwork.PLL.Views
         public void Show(User user) 
         {
             var receivedMessages = _messageService.GetReceivedMessages(user);
+
+            if (receivedMessages.Count() == 0)
+                Console.WriteLine("\nВходящих сообщений нет.\n");
+            else
             foreach (var receivedMessage in receivedMessages)
             {
                 Console.WriteLine($"\nId отправителя: {receivedMessage.sender_id}\n" +
